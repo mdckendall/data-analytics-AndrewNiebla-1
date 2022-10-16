6f5d492af5786d1caaeb1c7ef8eef067b3f00a6b
Andrew Niebla <113561001+AndrewNiebla@users.noreply.github.com>
2022-10-16 23:12:46 +0000

    Coded data analytics

--- Main.java
+++ Main.java
@@ -8,7 +8,43 @@ class Main {
 
 		ArrayList<String> arrayList = new ArrayList<>();
 		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
+	Scanner fileScanner = new Scanner(fr);
 		
-	}
-}
+
+   int UI = 0;
+    
+     Scanner userInput = new Scanner(System.in);
+    for(int i = 0; i < 50; i++){
+      arrayList.add(fileScanner.nextLine());
+    }
+    
+  while(UI != 5){
+     System.out.println("Press 1 to learn about salary.");
+  System.out.println("Press 2 to learn about the job.");
+  System.out.println("Press 3 to learn about demand.");
+  System.out.println("Press 4 to view current students.");
+  System.out.println("Press 5 to quit.");
+
+    UI = userInput.nextInt();
+    if(UI == 1){
+    System.out.println("$98,345 average salary.");
+  }else if(UI == 2){
+    System.out.println("US News - 100 Best Jobs!");
+  }else if(UI == 3){
+    System.out.println("Top 10 Forbes In-Demand Jobs!");
+  }else if(UI == 4){
+      System.out.println("Current Students:");
+    for(String Is : arrayList){
+      System.out.println(Is);
+    }
+    } else {
+    
+  }
+  
+  }
+userInput.close();
+  }
+  }
+
+
+  
